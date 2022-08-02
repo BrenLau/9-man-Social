b736be00fa6fa42e38c5584fa37137fc7f61ad11
BrenLau <brenl@DESKTOP-K01VFCG.localdomain>
2022-08-02 13:55:14 -0400

    again

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,33 +2,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <nav>
-      <img></img>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+    <nav className='nav'>
+      <img className='banner' src="https://brensteamupsproject.s3.amazonaws.com/bigger9mansocial.png"></img>
+      <ul className='navstuff'>
+        <li className='navli'>
+          <NavLink className='navli' to='/' exact={true} activeClassName='active'>
+            <img className='ball' src="https://pngimg.com/uploads/volleyball/volleyball_PNG1.png"></img>
+            <div className='labell'>Home</div>
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+        <li className='navli'>
+          <NavLink className='navli' to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+        <li className='navli'>
+          <NavLink className='navli' to='/sign-up' exact={true} activeClassName='active'>
             Sign-Up
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+        <li className='navli'>
+          <NavLink className='navli' to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </li>
-        <li>
+        <li className='navli'>
           <LogoutButton />
         </li>
       </ul>
