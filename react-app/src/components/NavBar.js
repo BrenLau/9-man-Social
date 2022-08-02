5a3625680af8c114f117dbfee7368356e77a2003
BrenLau <brenl@DESKTOP-K01VFCG.localdomain>
2022-08-02 12:26:07 -0400

    third

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -19,7 +19,7 @@ const NavBar = () => {
         </li>
         <li>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
+            Sign-Up
           </NavLink>
         </li>
         <li>
