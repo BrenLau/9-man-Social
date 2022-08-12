8aba992fe16f296a4f8ff48215ae900fc5b94779
BrenLau <brenl@DESKTOP-K01VFCG.localdomain>
2022-08-12 02:27:02 -0400

    navbar wont move with screen anymore

--- react-app/src/components/splash.js
+++ react-app/src/components/splash.js
@@ -7,7 +7,7 @@ const Splash = () => {
     return (
         <>
             <div className="balldiv">
-                <h2 id='splashh2'>This app is designed to be a place where the nineman community can get together and organize/communicate amongst eachother in one centralized location.</h2>
+                <h2 id='splashh2'>A place where the nineman community can get together and organize/communicate amongst eachother in one centralized location.</h2>
                 <img className='splashball' src="https://pngimg.com/uploads/volleyball/volleyball_PNG1.png"></img>
             </div>
             <img className='splashimage' src={image}></img>
