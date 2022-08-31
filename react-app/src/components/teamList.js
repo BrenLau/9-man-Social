622b0c0c279df3d2b464c1e02ecd795240fa38c0
BrenLau <brenl@DESKTOP-K01VFCG.localdomain>
2022-08-31 12:14:36 -0400

    splash message

--- react-app/src/components/teamList.js
+++ react-app/src/components/teamList.js
@@ -26,7 +26,7 @@ const TeamList = ({ setCurrentTeam, setHidden }) => {
                         await setCurrentTeam(team.id)
                         await dispatch(getPostsThunk(team.id))
                         await setHidden(false)
-                    }} key={team.id} to={`/teams/${team.id}`} activeClassName='activeteam' className="teamcapsule">{!team.image && team.name.split(' ')[0][0] + team.name.split(' ')[1][0]}</NavLink>
+                    }} key={team.id} to={`/teams/${team.id}`} activeClassName='activeteam' className="teamcapsule">{!team.image && team.name.split(' ')[0][0] + (team.name.split(' ')[1] && team.name.split(' ')[1][0])}</NavLink>
                 )
             })}
         </div>
